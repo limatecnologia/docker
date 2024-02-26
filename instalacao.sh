7b87c8f1c23abf22d5b34f5acc29a8afefbf4dd8
Jonathan Lima <lima_jonathan@hotmail.com>
2024-02-26 13:58:56 -0300

    commit

--- instalacao.sh
+++ instalacao.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
-clear
-echo "                                            Instalando o GIT"
-apt install -y git
+# Função para verificar se um comando está instalado
+check_command() {
+    command -v $1 >/dev/null 2>&1
+}
+
 clear
 
-echo "                                            Clonando repositorio"
+# Verifica se o GIT está instalado, e instala se necessário
+if ! check_command git; then
+    echo "Instalando o GIT..."
+    apt update
+    apt install -y git
+    clear
+fi
+
+# Verifica se o curl está instalado, e instala se necessário
+if ! check_command curl; then
+    echo "Instalando o curl..."
+    apt install -y curl
+    clear
+fi
+
+echo "Clonando repositório..."
 git clone https://github.com/limatecnologia/docker.git
 clear
 
-echo "                                            Acessando a pasta"
+echo "Acessando a pasta..."
 cd docker/
 
-echo "                                            Dando permissão ao arquivo"
+echo "Dando permissão ao arquivo..."
 chmod +x install_docker.sh
 clear
 
-echo "                                            Iniciando instalacao"
+echo "Iniciando instalação..."
 clear
 sleep 1
 ./install_docker.sh
